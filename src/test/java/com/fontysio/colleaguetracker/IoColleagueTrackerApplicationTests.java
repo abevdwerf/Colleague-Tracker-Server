b1e838babef1a3b6886cde857f8f285bcd67f1cf
Vermeire,Vincent V.L <v.vermeire@student.fontys.nl>
2022-09-27 14:30:30 +0200

    Initial Commit

---
+++ src/test/java/com/fontysio/colleaguetracker/IoColleagueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontysio.colleaguetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoColleagueTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
